42d7da342b02c3a7dde1283bfd1cc126cd1df12e
AussieGuy0 <anthony.bruno196@gmail.com>
2023-07-23 10:30:17 +0930

    .

--- src/main/java/dev/anthonybruno/htmx/Server.java
+++ src/main/java/dev/anthonybruno/htmx/Server.java
@@ -1,174 +1,50 @@
 package dev.anthonybruno.htmx;
 
 import io.javalin.Javalin;
-import io.javalin.http.HandlerType;
-import j2html.tags.specialized.LiTag;
-import j2html.tags.specialized.UlTag;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import j2html.tags.specialized.H2Tag;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.UUID;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static j2html.TagCreator.*;
 
-public class Server implements AutoCloseable {
+public class Server {
 
-  private static final Logger log = LoggerFactory.getLogger(Server.class);
-
-  /**
-   * Represents a single todo item.
-   */
-  public record Todo(String id, String content, boolean completed) {
+  // Create a H2 tag with an id.
+  private static H2Tag createCounterElement(int count) {
+    return h2("count: " + count)
+        .withId("counter");
   }
 
-  private final Javalin javalin;
+  public static void main(String[] args) {
+    var javalin = Javalin.create(config -> {
+          config.staticFiles.enableWebjars();
+        }
+    );
 
-
-  public Server() {
-    // In-memory 'todo' store.
-    var exampleTodo = new Todo(UUID.randomUUID().toString(), "Buy milk", false);
-    var idToTodo = new LinkedHashMap<String, Todo>();
-    idToTodo.put(exampleTodo.id, exampleTodo);
-
-    // Set up Javalin server.
-    javalin = Javalin.create(config -> {
-      config.staticFiles.enableWebjars();
-      config.staticFiles.add("public");
-    });
-
-    javalin.addHandler(HandlerType.GET, "/", ctx -> {
+    var counter = new AtomicInteger();
+    javalin.get("/", ctx -> {
       var content = html(
-        head(
-          script().withSrc("/webjars/htmx.org/1.9.2/dist/htmx.min.js"),
-          link().withRel("stylesheet").withHref("/style/pico.min.css")
-        ),
-        body(
-           div(
-            h1("Todo App"),
-            createTodoList(idToTodo)
-          ).withClass("container")
-        )
+          head(
+              script().withSrc("/webjars/htmx.org/1.9.2/dist/htmx.min.js")
+          ), (
+              body(
+                  h1("Hello world"),
+                  createCounterElement(counter.get()),
+                  button("Increment")
+                      .attr("hx-post", "/increment")
+                      .attr("hx-swap", "outerHTML")
+                      .attr("hx-target", "#counter")
+              )
+          )
       );
       var rendered = "<!DOCTYPE html>\n" + content.render();
-      ctx.header("Cache-Control", "no-store");
       ctx.html(rendered);
     });
 
-    // Create new todo item.
-    javalin.addHandler(HandlerType.POST, "/todos", ctx -> {
-      var newContent = ctx.formParam("content");
-      var newTodo = new Todo(UUID.randomUUID().toString(), newContent, false);
-      idToTodo.put(newTodo.id, newTodo);
-      ctx.html(createTodoList(idToTodo).render());
+    javalin.post("/increment", ctx -> {
+      var newCounter = createCounterElement(counter.incrementAndGet());
+      ctx.html(newCounter.render());
     });
-
-    // Update the content of a todo item.
-    javalin.addHandler(HandlerType.POST, "/todos/{id}", ctx -> {
-      var id = ctx.pathParam("id");
-      var newContent = ctx.formParam("value");
-
-      var updatedTodo = idToTodo.computeIfPresent(id, (_id, oldTodo) -> new Todo(id, newContent, oldTodo.completed));
-
-      ctx.html(createTodoItem(updatedTodo, false).render());
-    });
-
-    // Toggle the completed status of a specified todo item.
-    javalin.addHandler(HandlerType.POST, "/todos/{id}/toggle", ctx -> {
-      var id = ctx.pathParam("id");
-
-      var updatedTodo = idToTodo.computeIfPresent(id, (_id, oldTodo) -> new Todo(id, oldTodo.content, !oldTodo.completed));
-
-      ctx.html(createTodoItem(updatedTodo, false).render());
-    });
-
-    // Returns an 'edit' view of a todo item.
-    javalin.addHandler(HandlerType.POST, "/todos/{id}/edit", ctx -> {
-      var id = ctx.pathParam("id");
-
-      var todo = idToTodo.get(id);
-
-      ctx.html(createTodoItem(todo, true).render());
-    });
-
-    // Basic logging of requests/responses.
-    javalin.after((ctx) -> {
-      log.info("{} {} {}", ctx.req().getMethod(), ctx.path(), ctx.status());
-    });
-  }
-
-  private UlTag createTodoList(Map<String, Todo> idToTodo) {
-    return ul()
-      .withId("todo-list")
-      .with(
-        idToTodo.values().stream()
-          .map(todo -> createTodoItem(todo, false))
-      )
-      .with(
-        li(
-          form(
-            input()
-              .isRequired()
-              .withType("text")
-              .withName("content"),
-            input()
-              .withValue("Add")
-              .withType("submit")
-          )
-            .withStyle("display: flex;")
-            .attr("hx-swap", "outerHTML")
-            .attr("hx-target", "#todo-list")
-            .attr("hx-post", "/todos")
-        )
-       .withStyle("list-style-type: none")
-      );
-  }
-
-  public static LiTag createTodoItem(Todo todo, boolean editing) {
-    var text = div(todo.content)
-      .attr("hx-post", "/todos/" + todo.id + "/edit")
-      .withStyle("flex-grow: 1; cursor: text;")
-      .withCondStyle(todo.completed, "text-decoration: line-through; flex-grow: 1; cursor: text;");
-    var editInput = input()
-      .withValue(todo.content)
-      .withName("value")
-      .withType("text")
-      .withStyle("flex-grow: 2;")
-      .isAutofocus()
-      .attr("hx-post", "/todos/" + todo.id)
-      .attr("hx-target", "#todo-" + todo.id);
-    var completeCheckbox = (todo.completed ?
-      input()
-       .isChecked() :
-      input())
-      .withStyle("flex-basis: 0; min-width: 20px")
-      .withType("checkbox")
-      .withCondDisabled(editing)
-      .attr("hx-post", "/todos/" + todo.id + "/toggle");
-    return li(
-      completeCheckbox,
-      editing ? editInput : text
-    )
-      .attr("hx-target", "#todo-" + todo.id)
-      .attr("hx-swap", "outerHTML")
-      .withId("todo-" + todo.id)
-      .withStyle("display: flex; align-items: center;");
-  }
-
-  public void start() {
     javalin.start();
-  }
-
-  @Override
-  public void close() throws Exception {
-    javalin.close();
-  }
-
-  // Run the thing!
-  public static void main(String[] args) {
-    var server = new Server();
-    server.start();
   }
 }
--- src/test/java/dev/anthonybruno/htmx/ServerTest.java
+++ src/test/java/dev/anthonybruno/htmx/ServerTest.java
@@ -8,8 +8,6 @@ public class ServerTest {
 
   @Test
   void testHtml() {
-    var todo = new Server.Todo("id", "content", false);
-    var tag = Server.createTodoItem(todo, false);
-    assertThat(tag.getTagName()).isEqualTo("li");
+
   }
 }
